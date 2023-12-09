1c8a932ec8105e9e2b534768b94726ea519f1c4c
Dirk Arnez <smalldirkalex@gmail.com>
2023-12-09 18:32:34 +0800

    - ipdate run.cmd

--- run.cmd
+++ run.cmd
@@ -1,5 +1,5 @@
 git clone --depth 1 --no-checkout --sparse https://dirkarnez:xxx@github.com/dirkarnez/.git
 cd squareline-studio-portable
 git sparse-checkout set ".github"
-git checkout
+git checkout main
 cd ..
