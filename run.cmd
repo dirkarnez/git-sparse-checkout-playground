78a0c9f478e2c925075e25c29efb24a488ad5edf
Dirk Arnez <smalldirkalex@gmail.com>
2024-05-15 02:45:58 +0800

    - update run.cmd

--- run.cmd
+++ run.cmd
@@ -8,6 +8,7 @@ SET PATH=^
 git clone --depth 1 --sparse https://dirkarnez:****@github.com/dirkarnez/my-repo.git
 cd my-repo
 @REM git sparse-checkout can call multiple times
-git sparse-checkout add "1petrichhhor"
-git sparse-checkout add "4oiiww"
+@REM folder "./sd" should be express as "sd"
+git sparse-checkout add "abc"  
+git sparse-checkout add "def"
 pause
