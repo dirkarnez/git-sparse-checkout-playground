0b5c4d7bab130313a24a855f8fd21290cb4251bf
Dirk Arnez <smalldirkalex@gmail.com>
2024-05-15 02:45:04 +0800

    - upload files

--- run.cmd
+++ run.cmd
@@ -1,5 +1,13 @@
-git clone --depth 1 --no-checkout --sparse https://dirkarnez:xxx@github.com/dirkarnez/.git
-cd squareline-studio-portable
-git sparse-checkout set ".github"
-git checkout main
-cd ..
+@echo off
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+SET PATH=^
+%DOWNLOADS_DIR%\PortableGit\bin;
+
+
+git clone --depth 1 --sparse https://dirkarnez:****@github.com/dirkarnez/my-repo.git
+cd my-repo
+@REM git sparse-checkout can call multiple times
+git sparse-checkout add "1petrichhhor"
+git sparse-checkout add "4oiiww"
+pause
